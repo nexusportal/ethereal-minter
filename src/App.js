2c806846f0ed4f01c9a17a2c16ec2aec40402f2a
NEXUS <110637823+nexusportal@users.noreply.github.com>
2024-08-09 12:24:03 -0700

    update discord link

--- src/App.js
+++ src/App.js
@@ -282,7 +282,7 @@ function App() {
                     <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/PoweredByNEXUS">
                       <FaTwitter color="white" size={30} />
                     </a>
-                    <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/nexusportal">
+                    <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/nexusdefi">
                       <FaDiscord color="white" size={30} />
                     </a>
 
